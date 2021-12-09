31b79b7a0d806782b3a5d9dc865eee7d3e6a41c0
vilhok <kivihalme@gmail.com>
2021-12-10 00:09:03 +0200

    06/2018

--- src/solutions/year2018/Year2018Day06.java
+++ src/solutions/year2018/Year2018Day06.java
@@ -1,59 +1,174 @@
 package solutions.year2018;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+
 import com.github.aoclib.api.InputParser;
 import com.github.aoclib.solver.DayX;
 
 public class Year2018Day06 extends DayX {
 
+	record Point(int x, int y) {
+
+		public int manhattan(int x, int y) {
+			return Math.abs(x - this.x) + Math.abs(y - this.y);
+		}
+	}
+	/**
+	 * Operates under the assumption that an area that touches an edge will be infinite.
+	 */
+
 	@Override
 	public Object firstPart(InputParser input) {
-		/**
-		 * Calculate triangles of every three values. If a value is inside a triangle,
-		 * it's not infinite? If a value is outside a triangle, it's infinite. 19500
-		 * triangles? test each value against all triangles to determine the infinite
-		 * values. Then fill the area between min-value and max-value matrix with the
-		 * distances. Then calculate areas?
-		 * 
-		 * This might be unnecessary. If you just fill the area, all that touch the
-		 * border will be infinite. Based on reddit, no. Some proofs needed still, but
-		 * another alternative is to increase the area
-		 * 
-		 * 
-		 */
-		return NOT_SOLVED;
+		List<List<Integer>> pairs = input.linesAsLists(", ", Integer::parseInt);
+
+		int max = pairs.stream().flatMap(List::stream).mapToInt(i -> i).max().orElse(-1);
+
+		int bound = max + 1;
+
+		int[][] map = new int[bound][bound];
+
+		List<Point> points = new ArrayList<>();
+		Set<Integer> nonInfinite = new HashSet<>();
+		
+		int p = 1;
+		for (List<Integer> l : pairs) {
+			int x = l.get(1);
+			int y = l.get(0);
+			Point pnt = new Point(x, y);
+			points.add(pnt);
+			map[x][y] = p;
+			p++;
+		}
+
+		for (int x = 0; x < map.length; x++) {
+			for (int y = 0; y < map.length; y++) {
+				
+				if (map[y][x] == 0) { //for all empty locations
+					ArrayList<Integer> distances = new ArrayList<>();
+					int closest = Integer.MAX_VALUE;
+					int val = 0;
+					
+					//figure out distances to all points, along with the shortest
+					for (int i = 0; i < points.size(); i++) {
+						int dist = points.get(i).manhattan(y, x);
+						if (dist <= closest) {
+							closest = dist;
+							val = i + 1;
+						}
+						distances.add(dist);
+					}
+
+					//find out if there is more than one shortest distance.
+					final int value = closest;
+					long count = distances.stream().filter(i -> i == value).count();
+
+					if (count == 1) {
+						if (x == 0 || y == 0 || x == bound - 1 || y == bound - 1) {
+							//if this is at the edge, assume this is infinite.
+							nonInfinite.remove(val);
+						}
+						map[y][x] = val;
+					}
+				}
+			}
+		}
+		
+		boolean[][] visited = new boolean[bound][bound];
+
+		int maxArea = 0;
+		for (int x = 0; x < map.length; x++) {
+			for (int y = 0; y < map.length; y++) {
+				if (!visited[y][x] && nonInfinite.contains(map[y][x])) {
+					//BFS for all the non-infinite values to find the max area.
+					int area = calculateArea(map, visited, y, x, map[y][x]);
+					if (area > maxArea) {
+						maxArea = area;
+					}
+				}
+			}
+		}
+		return maxArea;
 	}
+
 
 	@Override
 	public Object secondPart(InputParser input) {
-		return NOT_SOLVED;
-	}
-	/**
-	 * 
-	@Override
-	public Object firstPart(InputParser input){
-		int[] allNumbers = input.asSingleIntArray(Delimiter.COMMA);
-		int max = Arrays.stream(allNumbers).max().getAsInt();
-		int[][] table = new int[max][max];
-		ArrayList<Point> points = new ArrayList<>();
-		for(int i = 0; i < allNumbers.length; i += 2){
-			int x = allNumbers[i];
-			int y = allNumbers[i + 1];
-			table[x][y] = i;
-			Point p = new Point(x, y);
-			points.add(p);
-		}
-		for(int i = 0; i < max; i++){
-			for(int j = 0; j < max; j++){
+		Function<String, Point> f = s -> {
+			String[] value = s.split(", ");
+			return new Point(Integer.parseInt(value[1]), Integer.parseInt(value[0]));
+		};
 
+		List<Point> points = input.<Point>getLines(f);
+
+		int theArea = 0;
+		for (Point p : points) {
+			int size = calculateAreaWithDistance(new HashSet<Point>(), p.x, p.y, points, 10000);
+			if (theArea == 0) {
+				theArea = size;
+			} else {
+				System.out.println("ERROR: two separate areas!");
 			}
 		}
-
-		return null;
+		return theArea;
 	}
+
+	public static int calculateArea(int[][] map, boolean[][] visited, int x, int y, int value) {
+		if (x < 0 || y < 0 || x >= map.length || y >= map[0].length) {
+			return 0;
+		}
+		if (visited[x][y]) {
+			return 0;
+		}
+		if (map[x][y] != value) {
+			return 0;
+		}
+		visited[x][y] = true;
+
+		int area = 1;
+		area += calculateArea(map, visited, x + 1, y, value);
+		area += calculateArea(map, visited, x - 1, y, value);
+		area += calculateArea(map, visited, x, y + 1, value);
+		area += calculateArea(map, visited, x, y - 1, value);
+
+		return area;
+	}
+
+	public static int calculateAreaWithDistance(Set<Point> visited, int x, int y, List<Point> points, int bound) {
+
+		if (visited.contains(new Point(x, y))) {
+			return 0;
+		}
+
+		int sum = points.stream().mapToInt(p -> p.manhattan(x, y)).sum();
+
+		if (sum >= bound)
+			return 0;
+		visited.add(new Point(x, y));
+
+		int area = 1;
+
+		area += calculateAreaWithDistance(visited, x + 1, y, points, bound);
+		area += calculateAreaWithDistance(visited, x - 1, y, points, bound);
+		area += calculateAreaWithDistance(visited, x, y + 1, points, bound);
+		area += calculateAreaWithDistance(visited, x, y - 1, points, bound);
+
+		return area;
+	}
+
 
 	@Override
-	public Object secondPart(InputParser input){
-		return NOT_SOLVED;
+	public void insertTestsPart1(List<Test> testcase) {
+		testcase.add(new Test("""
+				1, 1
+				1, 6
+				8, 3
+				3, 4
+				5, 5
+				8, 9
+				""", 17));
 	}
-	 */
 }
