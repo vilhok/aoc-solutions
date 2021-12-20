f6e15c7ce2e8e3abfb8eaf08db9fac8763c52541
vilhok <kivihalme@gmail.com>
2021-12-20 10:29:38 +0200

    day 20 year 2021

--- src/utils/Point.java
+++ src/utils/Point.java
@@ -2,4 +2,9 @@ package utils;
 
 public record Point(int x, int y) {
 	
+	
+	public Point add(Point other) {
+		return new Point(this.x+other.x,this.y+other.y);
+	}
+	
 }
--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -5,18 +5,29 @@ import java.util.List;
 
 public class Utils {
 
-
 	public static List<Point> fourNeighbors() {
 		return List.of(new Point(-1, 0), new Point(1, 0), new Point(0, 1), new Point(0, -1));
 	}
 
-	public List<Point> eightNeighbors() {
+	public static List<Point> eightNeighbors() {
 		ArrayList<Point> p = new ArrayList<>();
 		for (int x = -1; x <= 1; x++) {
 			for (int y = -1; y <= 1; y++) {
 				if (x != 0 && y != 0) {
 					p.add(new Point(x, y));
 				}
+			}
+		}
+		return p;
+	}
+
+	public static List<Point> nineNeighbors() {
+		ArrayList<Point> p = new ArrayList<>();
+		for (int y = -1; y <= 1; y++) {
+			for (int x = -1; x <= 1; x++) {
+
+				p.add(new Point(x, y));
+
 			}
 		}
 		return p;
